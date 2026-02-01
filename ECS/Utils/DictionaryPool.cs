c28901f80d3c11119e76d76de0cf835616e8c0a9
cardidi <cardidi@live.cn>
2026-02-01 14:43:36 +0800

    feat(core): optimize entity matcher changing performance consume.

---
+++ ECS/Utils/DictionaryPool.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace TinyECS.Utils
+{
+    public static class DictionaryPool<TKey, TValue>
+    {
+        private static readonly Pool<Dictionary<TKey, TValue>> _pool = new(
+            createFunc: () => new Dictionary<TKey, TValue>(),
+            returnAction: d => d.Clear());
+        
+        public static Dictionary<TKey, TValue> Get() => _pool.Get();
+        
+        public static Pool<Dictionary<TKey, TValue>>.PooledItemDisposable Get(out Dictionary<TKey, TValue> dict)
+            => _pool.Get(out dict);
+        
+        public static void Return(Dictionary<TKey, TValue> dict) => _pool.Release(dict);
+    }
+}
